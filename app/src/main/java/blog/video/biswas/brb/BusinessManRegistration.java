c4d128ca834738bd2e5c034628825991ee1dc30e
sushen <sushenbiswasaga@gmail.com>
2019-11-16 12:34:16 +0600

    You Tube Api Integrated.

---
+++ app/src/main/java/blog/video/biswas/brb/BusinessManRegistration.java
@@ -0,0 +1,14 @@
+package blog.video.biswas.brb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BusinessManRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_business_man_registration);
+    }
+}
